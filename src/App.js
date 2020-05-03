348f7083bc20ff4c948a3ff16afa87bbc36bbf43
Abhigna2202 <aparuvella@deakin.edu.au>
2020-05-04 09:30:39 +1000

    Initial commit with folder structure

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './../src/images/foodiesfeed.com_vegan-chickpea-and-potato-curry.jpg';
+import './App.css';
+
+export default () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1> Hey this is our food delight app</h1>
+      </header>
+    </div>
+    )
+}
+
