4530daff491ff32309271a64c8daebb8c2a431e5
yugandhar-dev <yugandharp20@gmail.com>
2020-06-17 22:56:42 +1000

    final commit frontend

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { useState } from 'react'
-import './App.css'
-import Signup from './components/SignUp/signup'
-import Login from './components/login/login'
-import Home from './components/Home/home'
+import React, { useState } from "react";
+import "./App.css";
+import Signup from "./components/SignUp/signup";
+import Login from "./components/login/login";
+import Home from "./components/Home/home";
 
 export default () => {
-  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false)
-  const [isRegister, setIsRegister] = useState(false)
-  const [username, setUsername] = useState(false)
-  const [authToken, setAuthToken] = useState('')
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+  const [isRegister, setIsRegister] = useState(false);
+  const [username, setUsername] = useState(false);
+  const [authToken, setAuthToken] = useState("");
 
   return (
     <div className="App">
@@ -23,7 +23,7 @@ export default () => {
       {!isUserLoggedIn && isRegister && (
         <Signup setIsRegister={setIsRegister} />
       )}
-      {isUserLoggedIn && <Home authToken={authToken} username={username}/>}
+      {isUserLoggedIn && <Home authToken={authToken} username={username} />}
     </div>
-  )
-}
+  );
+};
